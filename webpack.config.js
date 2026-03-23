6bda4aff164ce590f8b4d7773d24756fb4b3fb07
pahmaton <truhlyaev24@gmail.com>
2026-03-23 20:32:44 +0300

    #11 переделал конфиг

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,62 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 
-module.exports = {
-  target: 'web', 
-  mode: 'development',
-  
-  entry: './src/app.js', 
-  
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js', 
-    publicPath: '/',
-    clean: true,
-  },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
 
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-
-  devServer: {
-    port: 2033,
-    hot: true, 
-    liveReload: true,
-    historyApiFallback: true,
-    static: {
-      directory: path.join(__dirname, 'public'),
+  return {
+    target: 'web',
+    mode: isProduction ? 'production' : 'development',
+    
+    entry: './src/app.js', 
+    
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction ? 'bundle.[contenthash].js' : 'bundle.js', 
+      publicPath: '/',
+      clean: true,
     },
-    client: {
-      overlay: true, 
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
     },
-    proxy: [
-      {
-        context: ['/api'],
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-      },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        minify: isProduction ? {
+          removeComments: true,
+          collapseWhitespace: true,
+        } : false,
+      }),
     ],
-  },
+
+    devtool: isProduction ? false : 'eval-source-map',
+
+    devServer: {
+      port: 2033,
+      hot: false, 
+      liveReload: true,
+      historyApiFallback: true,
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      client: {
+        overlay: true, 
+      },
+      proxy: [
+        {
+          context: ['/api'],
+          target: 'http://localhost:8080',
+          changeOrigin: true,
+          secure: false,
+        },
+      ],
+    },
+  };
 };
